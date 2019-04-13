78c389926222fe2c20cdf17ef69f5bb9216c8970
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-04-13 16:30:19 -0300

    fix box and package;

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Dropzone from 'react-dropzone';
+import socket from 'socket.io-client';
 
 import { MdInsertDriveFile } from 'react-icons/md';
 
@@ -11,10 +15,35 @@ export default class Box extends Component {
   state = { box: {} };
 
   async componentDidMount() {
+    this.subscribeToNewFiles();
+
+
+
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
 
     this.setState({ box: response.data });
+  }
+
+  subscribeToNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket('https://ominweek-backend.herokuapp.com/');
+
+    io.emit('connectRoom', box);
+    io.on('file', data => {
+      this.setState({ box: {...this.state.box, files: [data, ...this.state.box.files]} });
+    });
+  }
+
+  handleUpload = (files) => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+
+      data.append('file', file);
+
+      api.post(`boxes/${box}/files`, data);
+    })
   }
 
   render() {
@@ -25,15 +54,27 @@ export default class Box extends Component {
           <h1>{this.state.box.title}</h1>
         </header>
 
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+
+              <p>Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
+
         <ul>
 
           { this.state.box.files && this.state.box.files.map(file => (
-            <li>
+            <li key={file._id}>
               <a className="fileInfo" href="{file.url}" target="_blank">
                 <MdInsertDriveFile size={24} color="#A5CFFF" />
                 <strong>{file.title}</strong>
               </a>
-              <span>{file.createdAt}</span>
+              <span>há{" "}{distanceInWords(file.createdAt, new Date(), {
+                locale: pt
+              })}</span>
             </li>
           ))}
         </ul>
